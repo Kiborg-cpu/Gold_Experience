15d509f47ad3ebf2a45e076884245dcf2ae14bce
UsurpER <stas_gol100x@mail.ru>
2023-11-19 00:59:23 +0300

    update base

--- Registr.py
+++ Registr.py
@@ -1,6 +1,42 @@
 import flet as ft
+import sqlite3
 
 
 class Registration(ft.Container):
-    def __init__(self):
+    def __init__(self, page):
         super().__init__()
+        self.page = page
+        self.login = ft.TextField(label='Придумайте никнейм', color=ft.colors.WHITE, width=400, height=70,
+                                  border_color=ft.colors.AMBER, border_radius=5)
+        self.nPassword = ft.TextField(label='Придумайте пароль', color=ft.colors.WHITE, width=400, height=70,
+                                      border_color=ft.colors.AMBER, border_radius=5)
+        self.create = ft.TextButton(text='Создать аккаунт', width=180,
+                                    height=30, on_click=self.create_account)
+        self.cancel = ft.TextButton(text='Вернуться на главную', width=180,
+                                    height=30, on_click=lambda _: self.page.go("/"))
+        self.content = ft.Column(wrap=True, controls=[self.login, self.nPassword, ft.Row([self.create, self.cancel])])
+
+    def create_account(self, arg):
+        database = sqlite3.connect("log_people")
+        cursor = database.cursor()
+
+        # Создаем таблицу 'users', если ее нет
+        cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+            login TEXT,
+            password TEXT
+        )""")
+        database.commit()
+
+        # Проверяем, существует ли уже логин
+        cursor.execute("SELECT login FROM users WHERE login = ?", (self.login.value,))
+        existing_login = cursor.fetchone()
+
+        if existing_login is None:
+            # Если логин не существует, вставляем нового пользователя
+            cursor.execute("INSERT INTO users VALUES (?, ?)", (self.login.value, self.nPassword.value))
+            database.commit()
+        else:
+            print(f'Такой логин уже есть: {self.login.value}')
+
+        # Закрываем соединение с базой данных
+        database.close()
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import flet as ft
 
+import Registr
+
 
 def main(page: ft.Page):
     page.title = "Flet counter example"
@@ -10,28 +12,6 @@ def main(page: ft.Page):
     def button_clicked(e):
         t.value = f"Textboxes values are:  '{login.value}', '{password.value}'."
         page.update()
-
-        if page.route == "/store":
-            page.views.append(
-                ft.View(
-                    "/store",
-                    [
-                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
-                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
-                    ],
-                )
-            )
-        page.update()
-
-    def registration(e):
-        page.route = "/registration"
-        page.controls.clear()
-        page.update()
-
-    t = ft.Text()
-
-    default = ft.TextField(label='Default', color=ft.colors.WHITE, width=400, height=70,
-                         border_color=ft.colors.AMBER, border_radius=5)
 
     login = ft.TextField(label='Логин', color=ft.colors.WHITE, width=400, height=70,
                          border_color=ft.colors.AMBER, border_radius=5)
@@ -47,13 +27,48 @@ def main(page: ft.Page):
     registr = ft.TextButton(text="Создать аккаунт",
                             width=180,
                             height=30,
-                            on_click=registration)
+                            on_click=lambda _: page.go("/registration"))
 
     page_login = ft.Container(
         content=ft.Column(wrap=True, controls=[login, password, ft.Row([b, registr])])
     )
 
-    page.add(page_login)
+    # def registration(e):
+    #    page.route = "/registration"
+    #    page.controls.clear()
+    #    page.controls.append()
+    #    page.update()
+
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                "/", controls=[page_login], vertical_alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            )
+        )
+        if page.route == "/registration":
+            page.views.append(
+                ft.View(
+                    "/registration",
+                    controls=[Registr.Registration(page)],
+                    vertical_alignment=ft.MainAxisAlignment.CENTER,
+                    horizontal_alignment=ft.CrossAxisAlignment.CENTER
+                )
+            )
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    t = ft.Text()
+
+    # page.add(page_login)
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
 
 
-ft.app(target=main)  # view=ft.WEB_BROWSER
+ft.app(target=main)#, view=ft.WEB_BROWSER)
